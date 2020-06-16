cb4e97ad5f4b4997459fd4e3c708d65ab6a737d4
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-06-16 15:51:41 +0300

    Updates

---
+++ precache-manifest.20631a7d4f78799d8bf499f87ee9987d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d755faf50718ee2927480659291498",
+    "url": "/react-hw-01/index.html"
+  },
+  {
+    "revision": "fdf07234ce1120ea4338",
+    "url": "/react-hw-01/static/css/main.f6d9db5a.chunk.css"
+  },
+  {
+    "revision": "2cc854a5a9c257bced91",
+    "url": "/react-hw-01/static/js/2.07622032.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01/static/js/2.07622032.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf07234ce1120ea4338",
+    "url": "/react-hw-01/static/js/main.f136d7fd.chunk.js"
+  },
+  {
+    "revision": "ce5b9338ca464dd85ff9",
+    "url": "/react-hw-01/static/js/runtime-main.bc703ba4.js"
+  }
+]);
